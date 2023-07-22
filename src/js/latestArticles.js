86e8fffacbda74ab0834d4f7e719a5a14f61ab63
Misha Panivnyk <panivnykm@gmail.com>
2023-07-22 14:00:28 +0300

    add js/latestArticles

---
+++ src/js/latestArticles.js
@@ -0,0 +1,45 @@
+ // Масив з даними про статті
+ import { articlesItems } from './latestArticlesItems';
+
+ // Функція для створення карточки
+ function createCard(article) {
+   const cardElement = document.createElement('div');
+   cardElement.classList.add('card');
+
+   const imgElement = document.createElement('img');
+   imgElement.src = article.img;
+   imgElement.classList.add('card-img');
+   imgElement.alt = article.title;
+   cardElement.appendChild(imgElement);
+
+   const titleElement = document.createElement('h3');
+   titleElement.textContent = article.title;
+   cardElement.appendChild(titleElement);
+
+   const dateElement = document.createElement('p');
+   dateElement.textContent = article.date;
+   cardElement.appendChild(dateElement);
+
+   const authorElement = document.createElement('p');
+   authorElement.textContent = `By ${article.author}`;
+   cardElement.appendChild(authorElement);
+
+   const commentsElement = document.createElement('p');
+   commentsElement.textContent = article.comments;
+   cardElement.appendChild(commentsElement);
+
+   return cardElement;
+ }
+
+ // Функція для створення всіх карточок і додавання їх у контейнер
+ function createAllCards() {
+   const cardsContainer = document.getElementById('latestArticles-container');
+
+   articlesItems.forEach((article) => {
+     const cardElement = createCard(article);
+     cardsContainer.appendChild(cardElement);
+   });
+ }
+
+ // Виклик функції для створення всіх карточок після завантаження сторінки
+ createAllCards();
---
+++ src/js/latestArticlesItems.js
@@ -0,0 +1,24 @@
+export const articlesItems = [
+    {
+      "img": "./images/Articles-1.jpg",
+      "title": "Things to Know When Choosing the Perfect Sofa",
+      "date": "05 May",
+      "author": "John Doe",
+      "comments": "9 Comments"
+    },
+    {
+      "img": "./images/Articles-2.jpg",
+      "title": "Things to Know When Choosing the Perfect Sofa",
+      "date": "23 Jun",
+      "author": "John Doe",
+      "comments": "3 Comments"
+    },
+    {
+      "img": "./images/Articles-3.jpg",
+      "title": "Things to Know When Choosing the Perfect Sofa",
+      "date": "15 May",
+      "author": "John Doe",
+      "comments": "6 Comments"
+    },
+  ];
+
